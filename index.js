e6271201e35ac24dc271a8f4cb61febf2d793bec
Gauravp7717 <114347888+Gauravp7717@users.noreply.github.com>
2024-08-06 15:42:10 +0530

    added some bulb function

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-console.log("practise")
+// const { DiCloud9 } = require("react-icons/di");
+
+
+var bulb = document.querySelector("#color");
+var btn =  document.querySelector("button");
+
+
+btn.addEventListener("click",function(){
+   
+    if(btn.innerHTML == "on"){
+        btn.addEventListener("click",function(){
+            btn.innerHTML = "off";
+            bulb.style.backgroundColor = "gray";
+        })
+    }
+    else{
+        btn.addEventListener("click",function(){
+            btn.innerHTML = "on";
+            bulb.style.backgroundColor = "yellow";
+    })
+}
+
+})
+
